b47d3b767e67228b4a554ceb3e6c7b4a0e950040
Shaker Al Muzaini <92122042+1shaker@users.noreply.github.com>
2022-06-22 23:54:53 +0300

    Create studendController.php

--- app/Http/Controllers/Studends/studendController.php
+++ app/Http/Controllers/Studends/studendController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Http\Controllers\Studends;  
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller; 
     
        
 class studendController extends Controller  
