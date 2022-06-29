0310137a9fac59c7e869d1864223554f4587a828
Shaker Al Muzaini <92122042+1shaker@users.noreply.github.com>
2022-06-29 18:57:37 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',static function () {
-    return view('welcome');
+    return view('welcome'); 
 });
 Route::Get('test', static function (){
     return view('p1');
