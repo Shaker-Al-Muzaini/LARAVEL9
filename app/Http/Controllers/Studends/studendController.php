325a1b4a9e37856420b0bb38eda8bd3bba80b5e3
Shaker Al Muzaini <92122042+1shaker@users.noreply.github.com>
2022-07-04 20:06:34 +0300

    Update studendController.php

--- app/Http/Controllers/Studends/studendController.php
+++ app/Http/Controllers/Studends/studendController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Http\Controllers\Studends;   
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller; 
 use App\Http\Requests\StudentRequest;
 use App\Models\studend;
 use Illuminate\Http\Request;
