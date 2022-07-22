f45bfa9ab162a9ec8f3a80e28cf509ceea46fabb
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-07-22 23:29:03 +0300

    Update p1.blade.php

--- resources/views/p1.blade.php
+++ resources/views/p1.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('bott')
-<h1>Shaker Almazini</h1>   
+<h1>Shaker Almazini</h1>    
 <h1 style="color: red">ابعت لغة</h1>
 {{--<h1>{{$n}}</h1>--}}
 {{--<h1>{{$b}}</h1>--}}
